7466aebc380f04d063a807692b1dff3e942e1b80
josephuk77 <69853298+josephuk77@users.noreply.github.com>
2023-05-14 15:40:13 +0900

    nextproject

---
+++ app/page.js
@@ -0,0 +1,17 @@
+import { connectDB } from '@/util/database';
+
+
+export default async function Home() {
+
+  const client = await connectDB;
+  const db = client.db("AnimalAnywhere");
+  let result = await db.collection('post').find().toArray();         
+  console.log(result);
+
+
+  return (
+   <div>
+      <h4>안녕</h4>
+   </div>
+  )
+}
---
+++ util/database.js
@@ -0,0 +1,17 @@
+import { MongoClient } from 'mongodb'
+
+const url = 'mongodb+srv://AnimalAnywhere:dltmddnr0820@cluster0.uouvz9l.mongodb.net/?retryWrites=true&w=majority'
+const options = { useNewUrlParser: true }
+let connectDB
+
+if (process.env.NODE_ENV === 'development') {                   //개발단계에서 웹페이지를 실행할 때 마다 DB에 연결하게 되면 안되기 때문에 변수에 저장해 한번만 접근하게 한다
+  if (!global._mongo) {                                                     
+    global._mongo = new MongoClient(url, options).connect()
+  }
+  connectDB = global._mongo
+} else {
+  connectDB = new MongoClient(url, options).connect()
+}
+export { connectDB }
+
+
