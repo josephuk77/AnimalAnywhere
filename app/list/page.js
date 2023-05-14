78dbfa0a521b57e6e7edac3d1fd4a2772f5bec1e
josephuk77 <69853298+josephuk77@users.noreply.github.com>
2023-05-14 16:11:46 +0900

    mongoDBconnect

---
+++ app/list/page.js
@@ -0,0 +1,26 @@
+import { connectDB } from "@/util/database";
+
+export default async function List() {
+
+    const client = await connectDB;
+    const db = client.db("AnimalAnywhere");
+    let result = await db.collection('post').find().toArray(); 
+    console.log(result);
+
+    return (
+      <div className="list-bg">
+        <div className="list-item">
+          <h4>글제목</h4>
+          <p>1월 1일</p>
+        </div>
+        <div className="list-item">
+          <h4>글제목</h4>
+          <p>1월 1일</p>
+        </div>
+        <div className="list-item">
+          <h4>글제목</h4>
+          <p>1월 1일</p>
+        </div>
+      </div>
+    )
+  }
