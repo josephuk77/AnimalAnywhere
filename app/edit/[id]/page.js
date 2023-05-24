df3493233f2ea7be78a845b1045e2d9d341e5397
josephuk77 <josephuk77@gmail.com>
2023-05-24 17:57:55 +0900

    cru 완성

--- app/edit/[id]/page.js
+++ app/edit/[id]/page.js
@@ -7,15 +7,16 @@ export default async function edit(props){
     const db = client.db("AnimalAnywhere");
     let result = await db.collection('post').findOne({_id: new ObjectId(props.params.id)}); 
     console.log(result);
-    await db.collection('post').updateOne({},{title : ''}); 
+  
 
     return(
         <div>
             <h4>글 수정</h4>
-            <form action="/api/post/new" method="POST">
+            <form action="/api/post/edit" method="POST">
                 <input name="title" defaultValue={result.title}/>
                 <input name="content" defaultValue={result.content}/>
-                <button type="sumit">버튼</button>
+                <input style={{display : 'none'}} name="_id" defaultValue={result._id.toString()} />
+                <button type="sumit">수정 완료</button>
             </form>
         </div>
     )
---
+++ pages/api/post/edit.js
@@ -0,0 +1,17 @@
+import { connectDB } from "@/util/database";
+import { ObjectId } from "mongodb";
+
+export default async function hadler(req, res){
+    if(req.method == 'POST'){
+        console.log(req.body);
+        
+        let editpost = {title : req.body.title, content: req.body.content}
+        const db = (await connectDB).db('AnimalAnywhere')
+        let result = await db.collection('post').updateOne(
+            {_id : new ObjectId(req.body._id)},
+            {$set : editpost}
+
+        )
+        res.status(200).redirect('/detail/'+req.body._id)
+    }
+}
