18edfde358e3e6498b77d799e46a1c43810f544e
juhyungrok <132822637+juhyungrok@users.noreply.github.com>
2023-06-06 20:01:17 +0900

    footer

--- app/signup/page.js
+++ app/signup/page.js
@@ -1,5 +1,5 @@
 import { connectDB } from "@/util/database";
-
+import Footers from "../tag/Footers";
 export default async function Home() {
   const client = await connectDB;
   const db = client.db("AnimalAnywhere");
@@ -113,6 +113,7 @@ export default async function Home() {
           </div>
         </div>
       </div>
+      <Footers />
     </div>
   );
 }
